0ddb54cc4eafe7cf5111c2b3295b5b9ecb7d2f13
shimauma0312 <shimabara-ryuji@outlook.jp>
2023-12-28 00:45:18 +0900

    絵文字テキストのコメントアウト化

--- js/get_emoji.js
+++ js/get_emoji.js
@@ -1,36 +1,51 @@
-/*
-*/
+"use strict";
 const url = "https://api.github.com/emojis";
-
 async function getEmoji() {
     try {
         const response = await fetch(url);
         const data = await response.json();
         displayEmoji(data);
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
     }
 }
-
 function displayEmoji(data) {
     const emojiList = document.getElementById("emoji_list");
+    if (!emojiList)
+        return;
     for (const [name, url] of Object.entries(data)) {
-        /* img */
         const emojiImage = document.createElement("img");
-        emojiImage.id = "emoji_contents";
+        emojiImage.id = "emoji_img";
         emojiImage.src = url;
         emojiImage.alt = name;
-        emojiImage.title = name;
-        emojiImage.class = "flex"
-        /* span */
-        const emojiName = document.createElement("span");
-        emojiName.textContent = name;
+        emojiImage.class = "flex";
 
-        /* imgとnameを統合 */
+        //TODO リテラルのみ引き出す正規表現修正
+        const strippedUrl = url.replace(/.*\/unicode\//, "").replace(/\.png\?v8/, "");
+        console.log(strippedUrl);
+        emojiImage.setAttribute("data-emoji-name", strippedUrl);
+
+        /* 絵文字タイトルテキスト */
+        // emojiImage.addEventListener("click", copy_name);
+        // const emojiName = document.createElement("span");
+        // emojiName.textContent = name;
+
+        /* element統合 */
         const emojiElement = document.createElement("li");
         emojiElement.appendChild(emojiImage);
-        emojiElement.appendChild(emojiName);
+        // emojiElement.appendChild(emojiName);
         emojiList.appendChild(emojiElement);
+    }
+}
+async function copy_name() {
+    const emojiName = String.fromCodePoint(parseInt(this.getAttribute("data-emoji-name"), 16));
+    if (!emojiName) return;
+    try {
+        await navigator.clipboard.writeText(emojiName);
+        console.log(`Copied ${emojiName} to clipboard`);
+    } catch (error) {
+        console.error('Failed to copy text: ', error);
     }
 }
 
