57daf014fd9154f2c10e9bacdacb124c70defde1
shimauma0312 <shimabara-ryuji@outlook.jp>
2023-12-28 09:17:31 +0900

    NaNだったらエラーを出力

--- js/emoji_controller.js
+++ js/emoji_controller.js
@@ -12,7 +12,13 @@ export async function copy_name(data_emoji_name) {
         console.error('data_emoji_name is not a string:', data_emoji_name);
         return;
     }
-    const emojiName = String.fromCodePoint(parseInt(extraction(data_emoji_name), 16));
+    const codePoint = parseInt(extraction(data_emoji_name), 16);
+    // NaNだったらエラーを出力
+    if (isNaN(codePoint)) {
+        console.error('Invalid code point:', codePoint);
+        return;
+    }
+    const emojiName = String.fromCodePoint(codePoint);
     if (!emojiName) return;
     try {
         await navigator.clipboard.writeText(emojiName);
